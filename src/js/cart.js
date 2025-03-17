75674ab84808a7b46111e68bf5e3402ab60fe524
vbarindelli <hae21002@byu.edu>
2025-03-17 23:54:23 +0100

    Individual activity week 2 - Total$ in cart

--- src/js/cart.js
+++ src/js/cart.js
@@ -4,6 +4,18 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  let cartTotal = 0;
+  if (cartItems) {
+    document.querySelector(".cart-footer").className = "show";
+
+    const cartPrices = cartItems.map((cartItem) => cartItem.quantity * cartItem.FinalPrice);
+    cartTotal = cartPrices.reduce((acc, num) => acc + num, 0);
+
+    let finalCart = document.querySelector(".cart-total");
+    finalCart.innerHTML = `Total: $${cartTotal.toFixed(2)}`;
+  }
+
+
 }
 
 function cartItemTemplate(item) {
@@ -18,11 +30,15 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: 1</p>
+  <p class="cart-card__quantity">qty: ${item.quantity}</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
   return newItem;
+
 }
 
 renderCartContents();
+
+
+
