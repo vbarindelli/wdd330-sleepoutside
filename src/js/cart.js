6552daa82e9e53adeb67da2d7db7301bba2dedc8
vbarindelli <hae21002@byu.edu>
2025-03-21 18:34:18 +0100

    product pages

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,18 +6,6 @@ function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
-  let cartTotal = 0;
-  if (cartItems) {
-    document.querySelector(".cart-footer").className = "show";
-
-    const cartPrices = cartItems.map((cartItem) => cartItem.quantity * cartItem.FinalPrice);
-    cartTotal = cartPrices.reduce((acc, num) => acc + num, 0);
-
-    let finalCart = document.querySelector(".cart-total");
-    finalCart.innerHTML = `Total: $${cartTotal.toFixed(2)}`;
-  }
-
-
 }
 
 function cartItemTemplate(item) {
@@ -32,15 +20,11 @@ function cartItemTemplate(item) {
     <h2 class="card__name">${item.Name}</h2>
   </a>
   <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-  <p class="cart-card__quantity">qty: ${item.quantity}</p>
+  <p class="cart-card__quantity">qty: 1</p>
   <p class="cart-card__price">$${item.FinalPrice}</p>
 </li>`;
 
   return newItem;
-
 }
 
 renderCartContents();
-
-
-
