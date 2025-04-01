4bd822390ccaf05e96593679d4a81939baa2cbf5
vbarindelli <hae21002@byu.edu>
2025-04-01 18:30:18 +0200

    first visit banner

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,17 @@
 import { loadHeaderFooter } from "./utils.mjs";
 
 loadHeaderFooter();
+
+
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+    document.querySelector(".banner").setAttribute('id', 'hide');
+}
+
+if (document.querySelector(".bannerClose")) {
+    document.querySelector(".bannerClose").addEventListener("click", function () {
+        this.closest(".banner").setAttribute('id', 'hide');
+    })
+}
+
+
